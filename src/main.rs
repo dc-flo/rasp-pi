e2fe112769a645e0aa5a398c61b73084c3179b79
flo <flo.joh.rock@gmail.com>
2025-03-16 00:42:21 +0100

    first gpio try

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::thread::sleep;
+use std::time::Duration;
+use rppal::gpio::{Gpio, Level};
+use rppal::system::DeviceInfo;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Blinking LED on {}", DeviceInfo::new()?.model());
+    let mut led = Gpio::new()?
+        .get(18)?
+        .into_output();
+    loop {
+        led.write(Level::High);
+        sleep(Duration::from_secs(1));
+        led.write(Level::Low);
+        sleep(Duration::from_secs(1));
+    }
 }
